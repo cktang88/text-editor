8ea78e2ef9071da9ddbc3ae7560207cace285ce6
cktang88 <arctangent20@yahoo.com>
2020-09-13 12:09:53 -0500

    tailwind

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # text-editor
 
 - TailwindCSS
+  - Next.js with TailwindCSS config files from https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss
 - Slate
 - Next.js with TypeScript
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   plugins: [
-    // ...
     "tailwindcss",
-    "autoprefixer",
-    // ...
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
   ],
 };
